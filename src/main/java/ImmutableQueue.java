01b9dbe32816d5e430e3702fa8b2bd99323fba14
marye <marye@paypal.com>
2020-01-03 18:31:46 +0800

    add immutablequeue

--- src/main/java/ImmutableQueue.java
+++ src/main/java/ImmutableQueue.java
@@ -1,12 +1,25 @@
 public class ImmutableQueue<T> implements Queue<T> {
+    final Stack<T> enqueueStack;
+    final Stack<T> dequeueStack;
+
+    public ImmutableQueue(Stack<T> es, Stack<T> ds) {
+        enqueueStack = es;
+        dequeueStack = ds;
+    }
+
     @Override
     public Queue<T> enQueue(T t) {
-        return null;
+        return new ImmutableQueue<T>(dequeueStack, enqueueStack.push(t));
     }
 
     @Override
     public Queue<T> deQueue() throws Exception {
-        return null;
+       if (isEmpty()) {
+           throw new Exception("This is a empty queue");
+       }
+       if (!dequeueStack.isEmpty()) {
+           return new ImmutableQueue<T>(dequeueStack.pop(), enqueueStack);
+       }
     }
 
     @Override
