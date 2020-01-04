06c06d93b2cc860ba3ad234ebf0e9bb43598da98
Martyn Ye <martynye@Martyns-MacBook-Pro.local>
2020-01-04 10:09:44 +0800

    implement immutablequeue

--- src/main/java/ImmutableQueue.java
+++ src/main/java/ImmutableQueue.java
@@ -19,16 +19,56 @@ public class ImmutableQueue<T> implements Queue<T> {
        }
        if (!dequeueStack.isEmpty()) {
            return new ImmutableQueue<T>(dequeueStack.pop(), enqueueStack);
+       } else if (enqueueStack.isEmpty()) {
+           return new EmptyQueue<>();
+       } else {
+           return new ImmutableQueue<>(reverseStack(enqueueStack), new ImmutableStack.EmptyStack<>());
        }
     }
 
     @Override
     public T head() throws Exception {
-        return null;
+        if (dequeueStack.isEmpty()) {
+            throw new Exception("This is a empty queue");
+        }
+        return dequeueStack.head();
     }
 
     @Override
     public boolean isEmpty() {
         return false;
+    }
+
+
+    private Stack<T> reverseStack(Stack<T> stack) throws Exception {
+        Stack<T> resStack = new ImmutableStack.EmptyStack<>();
+        while (!stack.isEmpty()) {
+            resStack.push(stack.head());
+            stack = stack.pop();
+        }
+        return resStack;
+    }
+
+
+    public static class EmptyQueue<T> implements Queue<T> {
+        @Override
+        public Queue<T> enQueue(T t) {
+            return new ImmutableQueue<T>(new ImmutableStack.EmptyStack<>(), new ImmutableStack<>(t, new ImmutableStack.EmptyStack<>()));
+        }
+
+        @Override
+        public Queue<T> deQueue() throws Exception {
+            throw new Exception("This is a empty queue");
+        }
+
+        @Override
+        public T head() throws Exception {
+            throw new Exception("This is a empty queue");
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return true;
+        }
     }
 }
